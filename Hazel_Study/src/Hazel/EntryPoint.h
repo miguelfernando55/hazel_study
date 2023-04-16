e469f7a6e049918019c56728e13887ee40d3087f
miguelfernando55 <miguelfernando_55@hotmail.com>
2023-04-16 22:51:30 +0100

    Premake done

--- Hazel_Study/src/Hazel/EntryPoint.h
+++ Hazel_Study/src/Hazel/EntryPoint.h
@@ -6,7 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine!\n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized level");
+	int a = 5;
+	HZ_INFO("Variable a = {0}", a);
+
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
